bb99e0035d8e7a5b6f2479e93370c31eed9d2af8
Minh Thanh <mix.kris97@gmail.com>
2019-05-22 17:37:33 +0700

    upload page 404

---
+++ src/component/Page404/page404.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './page404.css';
+
+const Page404 = () => {
+  return (
+    <div id="notfound">
+      <div className="notfound">
+        <div className="notfound-404">
+          <h1>Oops!</h1>
+        </div>
+        <h2>404 - Page not found</h2>
+        <p>
+          The page you are looking for might have been removed had its name
+          changed or is temporarily unavailable.
+        </p>
+        <a href="/">Go To Homepage</a>
+      </div>
+    </div>
+  );
+};
+
+export default Page404;
--- src/container/App/App.js
+++ src/container/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch, Redirect, BrowserRouter } from 'react-router-dom';
 import RegisterForm from '../../container/Form/RegisterForm/registerForm';
 import LoginForm from '../../container/Form/LoginForm/loginForm';
+import Page404 from '../../component/Page404/page404';
 function App() {
   return (
     <div className="App">
@@ -9,7 +10,12 @@ function App() {
         <Switch>
           <Route exact path="/register" component={RegisterForm} />
           <Route exact path="/login" component={LoginForm} />
-          <Route exact path="/" component={LoginForm} />
+          <Route exact path="/" component={Page404} />
+          <PrivateRoute
+            path="/manage/products"
+            // component={HomePageAdminEvent}
+          />
+          <Route component={Page404} />
         </Switch>
       </BrowserRouter>
       {/* <RegisterForm /> */}
@@ -18,5 +24,18 @@ function App() {
     </div>
   );
 }
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      localStorage.isUserAuthenticated === 'true' ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+);
 
 export default App;
